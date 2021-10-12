10752007b296b6c5fca4e36643640bf3cd543be6
Aliona Petrova <petrova.alja@yandex.ru>
2021-10-12 23:37:37 +0300

    rm unused imports

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import serial
 import re
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
-import time
 import datetime as dt
 
 # Max amount of points displaying on the chart
